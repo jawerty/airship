7abad3e6503e6eda5d6ed70b7621244fc761c88e
Jared Wright <jtw57@gmail.com>
2015-02-01 15:49:36 -0500

    device discovery

--- js/main.js
+++ js/main.js
@@ -1,8 +1,30 @@
 console.log("Main app starting")
 
+var device_names = {};
+var updateDeviceName = function(device) {
+  device_names[device.address] = device.name;
+};
+var removeDeviceName = function(device) {
+  delete device_names[device.address];
+}
+
+// Add listeners to receive newly found devices and updates
+// to the previously known devices.
+chrome.bluetooth.onDeviceAdded.addListener(updateDeviceName);
+chrome.bluetooth.onDeviceChanged.addListener(updateDeviceName);
+chrome.bluetooth.onDeviceRemoved.addListener(removeDeviceName);
+
 chrome.bluetooth.getDevices(function(devices) {
-  console.log("searching for devices")
+  console.log("Devices: "+devices)
   for (var i = 0; i < devices.length; i++) {
     console.log(devices[i].address);
   }
+});
+
+chrome.bluetooth.startDiscovery(function() {
+  // Stop discovery after 30 seconds.
+  console.log("searching for devices")
+  setTimeout(function() {
+    chrome.bluetooth.stopDiscovery(function() {});
+  }, 1000000);
 });
