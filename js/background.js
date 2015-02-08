7d55d1b60ad84c52b197b19538bf00a21d7802c4
Jared Wright <jtw57@gmail.com>
2015-02-08 15:43:51 -0500

    working local network

--- js/background.js
+++ js/background.js
@@ -1,10 +1,27 @@
 console.log("Background app started")
 
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('index.html', {
-    bounds: {
-      width: 600,
-      height: 400
-    }
-  });
+  new socketHandler();
 });
+
+
+var socketHandler = function() {
+  var screenWidth = screen.availWidth;
+  var screenHeight = screen.availHeight;
+  var width  = 1200;
+  var height = 860;
+
+  chrome.app.window.create(
+    'index.html', {
+      id: "Airship",
+      minWidth:  600,
+      minHeight: 600,
+      state: "normal",
+    },
+    
+    function(win) {
+      win.onClosed.addListener(function() {
+      });
+    }
+  );
+}
--- js/main.js
+++ js/main.js
@@ -1,30 +1,98 @@
-console.log("Main app starting")
-
-var device_names = {};
-var updateDeviceName = function(device) {
-  device_names[device.address] = device.name;
-};
-var removeDeviceName = function(device) {
-  delete device_names[device.address];
+function ab2str(buf) {
+  return String.fromCharCode.apply(null, new Uint16Array(buf));
 }
 
-// Add listeners to receive newly found devices and updates
-// to the previously known devices.
-chrome.bluetooth.onDeviceAdded.addListener(updateDeviceName);
-chrome.bluetooth.onDeviceChanged.addListener(updateDeviceName);
-chrome.bluetooth.onDeviceRemoved.addListener(removeDeviceName);
-
-chrome.bluetooth.getDevices(function(devices) {
-  console.log("Devices: "+devices)
-  for (var i = 0; i < devices.length; i++) {
-    console.log(devices[i].address);
+function str2ab(str) {
+  var buf = new ArrayBuffer(str.length*2); // 2 bytes for each char
+  var bufView = new Uint16Array(buf);
+  for (var i=0, strLen=str.length; i < strLen; i++) {
+    bufView[i] = str.charCodeAt(i);
   }
-});
+  return buf;
+}
 
-chrome.bluetooth.startDiscovery(function() {
-  // Stop discovery after 30 seconds.
-  console.log("searching for devices")
-  setTimeout(function() {
-    chrome.bluetooth.stopDiscovery(function() {});
-  }, 1000000);
-});
+function saveToDisk(fileUrl, fileName) {
+    var save = document.createElement('a');
+    save.href = fileUrl;
+    save.target = '_blank';
+    save.download = fileName || fileUrl;
+
+    var event = document.createEvent('Event');
+    event.initEvent('click', true, true);
+
+    save.dispatchEvent(event);
+    (window.URL || window.webkitURL).revokeObjectURL(save.href);
+    console.log(save)
+}
+
+function onReadAsDataURL(event, text) {
+    var data = {}; // data object to transmit over data channel
+
+    if (event) text = event.target.result; // on first invocation
+    
+    if (text.length > chunkLength) {
+        data.message = text.slice(0, chunkLength); // getting chunk using predefined chunk length
+    } else {
+        data.message = text;
+        finished = true;
+    }
+
+    console.log(data.message)
+	var messageBuffer = str2ab(data.message)
+
+	console.log("Sending...")
+	chrome.storage.local.get('socketId', function (result) {
+		console.log(result.socketId)
+	    chrome.sockets.udp.send(result.socketId, messageBuffer, "10.0.0.139", 8080, function(sendInfo){
+			console.log("Send Info: "+JSON.stringify(sendInfo));
+			var remainingDataURL = text.slice(data.message.length);
+		    if (remainingDataURL.length)  onReadAsDataURL(null, remainingDataURL);
+		});    
+    });
+
+}
+
+function readVideoFile(file) {
+	var reader = new window.FileReader();
+	reader.readAsDataURL(file);
+
+	fileName = file.name;
+	reader.onload = onReadAsDataURL;
+
+	chunkLength = 1000;
+}
+
+
+
+function networkConnection() {
+	finished = false;
+	arrayToStoreChunks = [];
+
+	chrome.sockets.udp.create({name:"airship"}, function(createInfo) {
+		console.log(createInfo)
+		chrome.sockets.udp.bind(createInfo.socketId, "10.0.0.139", 8080, function(result) {
+
+			chrome.sockets.udp.onReceive.addListener(function(info) {
+				if (info.socketId != createInfo.socketId) return;
+				arrayToStoreChunks.push(ab2str(info.data));
+				console.log("Received Message");
+				if (finished == true) {
+					console.log("Finished")
+					saveToDisk(arrayToStoreChunks.join(''), fileName)
+					delete fileName;
+					arrayToStoreChunks = [];
+				}
+				
+			});
+
+			$("#chooseFile").change(function(e) {
+				chrome.storage.local.set({'socketId': createInfo.socketId}, function() {
+		          readVideoFile(e.target.files[0]);
+		        });
+				
+			});
+		});
+	});
+}
+
+ networkConnection()
